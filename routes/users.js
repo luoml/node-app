7abed82d3e0efd044704d566ec116628674ede4e
mingliluo <ml_luo@126.com>
2021-06-09 22:08:05 +0800

    账号注册，数据写入mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,50 @@
 const express = require("express");
+const mongoose = require('mongoose');
 
 const router = express.Router();
+
+require("../models/User");
+const User = mongoose.model('users');
 
 // 注册
 router.get("/register", (req, res) => {
     res.render("users/register");
+})
+
+router.post("/register", (req, res) => {
+    // console.log(req.body);
+    let errors = [];
+    if (req.body.password.length < 4) {
+        errors.push({text: "密码长度不能小于4位！"});
+    }
+    if (req.body.password != req.body.password2) {
+        errors.push({text: "两次密码不一致！"});
+    }
+    
+    if (errors.length > 0) {
+        res.render("users/register", {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });
+    } else {
+        const newUser = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }
+        new User(newUser).save().then(user => {
+            req.flash("successMsg", "账号注册成功");
+            res.redirect('/users/login');
+        }).catch(err => {
+            req.flash("errorMsg", "账号注册失败");
+            res.redirect('/users/register');
+            console.err(err);
+        });
+    }
+
 })
 
 // 登录
